7851eddbb86ddfb60bf6e4eb8fd6b52cadd80170
sasa <forexbotiam@gmail.com>
2025-06-07 16:15:34 -0500

    3

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,17 @@
 import { MongoClient } from 'mongodb';
 
 declare global {
+  // MongoDB
   var _mongoClientPromise: Promise<MongoClient> | undefined;
+
+  // MiniKit
+  interface MiniKitGlobal {
+    isInstalled?: () => Promise<boolean>;
+  }
+
+  interface Window {
+    MiniKit?: MiniKitGlobal;
+  }
 }
 
 export {};
